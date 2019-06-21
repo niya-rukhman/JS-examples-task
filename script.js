75a19f013a061040a01e5df41048102c9b1cd66e
niya-rukhman <49750829+niya-rukhman@users.noreply.github.com>
2019-06-21 12:55:17 +0500

    added files

---
+++ script.js
@@ -0,0 +1,149 @@
+
+// EXAMPLE NO. 1
+/*let carSpeed = 70;
+if (carSpeed > 60){
+    alert("High Speed");
+}
+else{
+    alert("Not");
+}*/
+
+// ----------------
+
+// EXAMPLE NO.2
+
+/*let isLoggedin = true;
+if (isLoggedin){
+    alert("Welcome Nida");
+}
+else{
+    alert("Please log in to continue!")
+}*/
+
+// ------------------
+
+// EXAMPLE NO.3
+/*
+let temperature = 45;
+if (temperature > 40){
+    alert("IMPORTANT ANNOUNCEMENT!!! UIT is closed tomorrow due to heat wave.");
+}*/
+
+// -------------------
+
+// EXAMPLE NO. 4
+
+/*let personHasRs = 3000000;
+if (personHasRs = 1000000){
+    alert("You Are A Millionaire");
+}
+else{
+    alert("NOT");
+}*/
+
+// ------------------
+
+// EXAMPLE NO.5
+
+/*let isElectric = true;
+if(isElectric){
+    alert("Car Is Manufactured By TESLA");
+}
+
+if(isElectric){
+    console.log("Car Is Manufactured By TESLA");
+}*/
+
+// ----------------
+
+// EXAMPLE NO. 6
+
+/*let userScore = 90;
+let currentHighScore = 70;
+if (userScore > currentHighScore){
+    alert("Congratulations!! Your new high score is 90");
+}
+else{
+  alert("Try Again. You just need 20 to win!")
+}*/
+
+// ---------------
+
+// EXAMPLE NO. 7
+
+/*let isEven = true;
+if (isEven){
+    alert("EVEN");
+}
+else{
+    alert("ODD");
+}*/
+
+/*let evenNumber = 6;
+if (evenNumber){
+    alert("EVEN");
+}
+else{
+    alert("ODD");
+}*/
+
+// --------------
+
+// EXAMPLE NO. 8
+
+/*let callerHasRs = 15;
+if (callerHasRs > 10){
+    alert("Ring Ring");
+}
+else if(callerHasRs < 1){
+    alert("Moazziz Sarif, aap ka mojooda balance iss call k lye naa kaafi hai. Please re-charge karain");
+}
+else{
+    alert("Moazziz sarif, aap ka balance khatam honay wala hai , and then prints Ring Ring!!");
+}*/
+
+// --------------
+
+// EXAMPLE NO. 9
+/*let isPassed = true;
+let mathScore = 15;
+if(isPassed){
+    alert("passed");
+}
+else{
+    alert("NOT");
+}*/
+// NOT WORKING
+/* let isPassed = true;
+ mathScore = 15;
+  compScore = 19;
+engScore = 7;
+ if(mathScore > 10) || (compScore < 20) || (engScore < 8){
+     alert(PASSED)
+
+ }*/
+
+//  -------------
+
+// EXAMPLE NO. 10
+/*let isPassed = true;
+mathScore = 15;
+compScore = 27;
+engScore = 9;
+if(isPassed){
+    alert("PASSED");
+}
+else{
+    alert("NOT");
+}*/
+
+// -------------
+
+/*
+
+me eg.no.9 me "OR" eg.no.10 me"AND" lagana charahi hu magar nahi huparaha.*/
+
+
+
+
+
